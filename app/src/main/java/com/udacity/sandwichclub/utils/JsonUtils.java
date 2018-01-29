128cc573154aa7d62089e136420204745553ab4e
ultrajustin22 <howejust@gmail.com>
2018-01-29 16:17:56 +0800

    Initial commit

---
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,67 @@
+package com.udacity.sandwichclub.utils;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
+
+    private static final String LOG_TAG = JsonUtils.class.getName();
+
+    public static Sandwich parseSandwichJson(String json) {
+
+        String mainName;
+        List<String> alsoKnownAs;
+        String placeOfOrigin;
+        String description;
+        String imageURL;
+        List<String> ingredientsList;
+
+        //First one must assume that the JSON string may be empty or invalid, so return null if matches with those cases
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+        try {
+            //Perhaps I will need another "for loop" around here...
+            JSONObject rootObject = new JSONObject();
+            JSONObject nameJSONObject = rootObject.getJSONObject("name");
+            mainName = nameJSONObject.getString("name");
+            JSONArray aliasJSONArray = nameJSONObject.getJSONArray("alsoknownas");
+            alsoKnownAs = new ArrayList<String>();
+            for (int i = 0; i < aliasJSONArray.length(); i++) {
+                String extraName = aliasJSONArray.getString(i);
+                alsoKnownAs.add(extraName);
+            }
+
+            //Might need more information within the mainName Object
+            placeOfOrigin = rootObject.getString("placeOfOrigin");
+            description = rootObject.getString("description");
+            imageURL = rootObject.getString("image");
+
+            JSONArray ingredientsJSONArray = rootObject.getJSONArray("ingredients");
+            ingredientsList = new ArrayList<String>();
+            for (int i = 0; i < ingredientsJSONArray.length(); i++) {
+                //Check logic
+                String ingredient = ingredientsJSONArray.getString(i);
+                ingredientsList.add(ingredient);
+            }
+
+            Sandwich sandwichObject = new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,imageURL,ingredientsList);
+            return sandwichObject;
+        }
+        catch (JSONException e) {
+            Log.e(LOG_TAG, "Problem obtaining results" + e);
+        }
+
+        //Otherwise return nothing
+        return null;
+    }
+}
